bfe6da3497282c07522544a2f7c49dbeb1b46e17
V714 <39555713+V714@users.noreply.github.com>
2018-06-01 23:31:55 +0200

    Update vninegag.py

--- vninegag.py
+++ vninegag.py
@@ -9,25 +9,28 @@ iasf='https://img-9gag-fun.9cache.com/photo/'
 
 ### Taking page html source to string
 def pageToStr(url):
+    
+
+### Getting link of gag from 1st position
+def getGag():
     a = urllib.request.urlopen(url)
     enc = a.headers.get_content_charset("utf-8")
     d = a.read()
-    return d.decode(enc)
-    
-### Returning title as a string.
-def getTitle():
-    b = pageToStr(ninegag)
+    b = d.decode(enc)
     c = b.find(wWSf)+45
     ce= len(b)-c-7
     gag = b[c:-ce]
-    link = ninegag+"gag/"+gag
+    return ninegag+"gag/"+gag
+
+### Returning title as a string.
+def getTitle(link):
     b = pageToStr(link)
     c = b.find("<title>")+7
     d = len(b)-b.find("</title>")+7
     return str(b[c:-d])
 
 ### Returning link to content (gif/photo/video) as a string.
-def getLink():
+def getLink(link):
     b = pageToStr(ninegag)
     c = b.find(wWSf)+45
     ce= len(b)-c-7
